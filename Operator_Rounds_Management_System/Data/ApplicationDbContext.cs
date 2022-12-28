9110614302f987b0690537aeaef3045d927dc8a6
Bart Lyons <lyons.bart@protonmail.com>
2022-12-27 19:22:19 -0500

    Added AppUser, and Skill models.

--- Operator_Rounds_Management_System/Data/ApplicationDbContext.cs
+++ Operator_Rounds_Management_System/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Operator_Rounds_Management_System.Models;
 
 namespace Operator_Rounds_Management_System.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ Operator_Rounds_Management_System/Models/AppUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Operator_Rounds_Management_System.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "Your {0} cannot be longer than {1} or shorter than {2} characters."), MinLength(2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "Your {0} cannot be longer than {1} or shorter than {2} characters."), MinLength(2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public virtual ICollection<int>? EmployeeId { get; set; }
+
+
+        public virtual ICollection<Skill>? Skill { get; set; }
+    }
+}
