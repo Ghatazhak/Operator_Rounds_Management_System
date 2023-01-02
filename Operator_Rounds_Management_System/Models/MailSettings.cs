35b14a1bbf1bb2914a8a3242d164863a371b490f
Bart Lyons <lyons.bart@protonmail.com>
2023-01-02 08:29:14 -0500

    Added some mail functionality.

---
+++ Operator_Rounds_Management_System/Models/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Operator_Rounds_Management_System.Models
+{
+    public class MailSettings
+    {
+        public string? Email { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Password { get; set; }
+        public string? Host { get; set; }
+        public int Port { get; set; }
+    }
+}
